82ab98674f061a518a9cc0572c067d21522ac4bc
Mitchhoff12 <112774103+Mitchhoff12@users.noreply.github.com>
2025-05-05 16:44:56 -0500

    Add files via upload

---
+++ paintball_game_code/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+C_SRCS += \
+../elec327.c \
+../paintball1.c \
+../startup_mspm0g350x_ticlang.c 
+
+C_DEPS += \
+./elec327.d \
+./paintball1.d \
+./startup_mspm0g350x_ticlang.d 
+
+OBJS += \
+./elec327.o \
+./paintball1.o \
+./startup_mspm0g350x_ticlang.o 
+
+OBJS__QUOTED += \
+"elec327.o" \
+"paintball1.o" \
+"startup_mspm0g350x_ticlang.o" 
+
+C_DEPS__QUOTED += \
+"elec327.d" \
+"paintball1.d" \
+"startup_mspm0g350x_ticlang.d" 
+
+C_SRCS__QUOTED += \
+"../elec327.c" \
+"../paintball1.c" \
+"../startup_mspm0g350x_ticlang.c" 
+
+
